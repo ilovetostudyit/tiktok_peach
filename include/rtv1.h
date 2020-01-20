0ec5a01818494c4e11afbf7a0aebfd51daadc70e
Ezzelyno Haggon <ehaggon@vo-b2.21-school.ru>
2020-01-20 16:37:37 +0300

    include change

--- include/rtv1.h
+++ include/rtv1.h
@@ -6,8 +6,14 @@
 /*   By: ehaggon <ehaggon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:18:29 by ehaggon           #+#    #+#             */
-/*   Updated: 2020/01/20 16:25:23 by ehaggon          ###   ########.fr       */
+/*   Updated: 2020/01/20 16:37:12 by ehaggon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+# include <stdbool.h>
+# include <SDL_image.h>
+# include <math.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
